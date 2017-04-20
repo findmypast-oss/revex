039cfada27f32ad887b35dc27725ddff0a104fe8
Patrick McDonnell <pmcdonnell@findmypast.com>
2017-04-20 10:12:53 +0100

    Initialising repo with skeleton mix project. Paired: Steven

---
+++ lib/revex.ex
@@ -0,0 +1,18 @@
+defmodule Revex do
+  @moduledoc """
+  Documentation for Revex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Revex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/revex_test.exs
@@ -0,0 +1,8 @@
+defmodule RevexTest do
+  use ExUnit.Case
+  doctest Revex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
